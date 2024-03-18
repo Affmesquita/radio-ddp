02f5e65fc22dcc536ef361ef1666117ca64377e2
Affonso Mesquita <102181682+Affmesquita@users.noreply.github.com>
2024-03-18 17:34:51 -0300

    atualização RD

--- javaScript/radioStation.js
+++ javaScript/radioStation.js
@@ -1,1 +1,12 @@
-const RadioStation = require('radio-station')
+const { create } = require('radio-station') 
+
+const radio = await create({
+    pathWorkDir: `${__dirname}/tracks-data-folder`,
+    isLauncher: true,
+    port: 9933,
+    login: 'localhost',
+    password: 'hackme',
+  })
+
+  
+  
