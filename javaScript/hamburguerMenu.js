f431e86a5804c68d656344a5fd8cc495198ab598
Affonso Mesquita <102181682+Affmesquita@users.noreply.github.com>
2024-06-25 19:46:25 -0300

    update 250624

--- javaScript/hamburguerMenu.js
+++ javaScript/hamburguerMenu.js
@@ -1,9 +1,29 @@
 const btnMenu = document.getElementById("btn-hamburger")
 
+btnMenu.classList.add('inactive')
+
 export function toggleMenu() {
     const nav = document.getElementById('header-list')
     nav.classList.toggle('active')
 }
 
 btnMenu.addEventListener('click', toggleMenu)
+
+/*
+const btnMenu = document.getElementById("btn-hamburger");
+
+// Adiciona classe inicial ao botão hamburguer
+btnMenu.classList.add('inactive');
+
+export function toggleMenu() {
+    const nav = document.getElementById('header-list');
+    nav.classList.toggle('active');
+}
+
+btnMenu.addEventListener('click', function() {
+    btnMenu.classList.remove('inactive'); // Remove a classe 'inactive' ao ser clicado
+    toggleMenu();
+});
+*/
+
 
