f5a200e7028145efb9b07bf8453a31ea25e6d32f
Affonso Mesquita <102181682+Affmesquita@users.noreply.github.com>
2024-10-18 12:37:17 -0300

    update 181024 static routes

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('node:path')
+const route  = require('./src/routers/staticRoutes')
+const app = express()
+
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'src', 'views'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.use(route)
+
+const PORT = 3000
+app.listen(PORT, console.log('SERVER DDP ON http://localhost:3000'))
+//        node server.js        // 
---
+++ src/routers/staticRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const router = express.Router()
+
+router.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'))
+})
+
+router.get('/noticias', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'news.html'))
+})
+
+router.get('/sobre', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'sobre.html'))
+})
+
+
+
+module.exports = router
