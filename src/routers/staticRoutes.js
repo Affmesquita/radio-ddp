58323f0210de370c189a398afde0ccab05d28c84
Affonso Mesquita <102181682+Affmesquita@users.noreply.github.com>
2024-10-19 19:16:54 -0300

    update 191024 pesquisa e correções

--- src/routers/staticRoutes.js
+++ src/routers/staticRoutes.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const path = require('path')
 const router = express.Router()
-/*
+
 router.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'))
+    res.sendFile(path.join(__dirname, '..', '..', 'public', 'html', 'index.html'))
 })
 
 router.get('/noticias', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html', 'news.html'))
+    res.sendFile(path.join(__dirname, '..', '..', 'public', 'html', 'news.html'))
 })
 
 router.get('/sobre', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html', 'sobre.html'))
+    res.sendFile(path.join(__dirname, '..', '..', 'public', 'html', 'sobre.html'))
 })
 
-*/
+
 
 module.exports = router
