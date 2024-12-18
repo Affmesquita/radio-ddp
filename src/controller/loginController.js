893ed7e124a9a2c3bd9dc0b7603ab25ae8a69283
Affonso Mesquita <102181682+Affmesquita@users.noreply.github.com>
2024-12-18 10:34:03 -0300

    update 181224 process p1

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,25 +1,26 @@
 const jwt = require('jsonwebtoken')
 
-const { USERNAME, PASSWORD_HASH, SECRET_KEY } = require('../config/config');
+const { USERNAME, PASSWORD_HASH, SECRET_KEY } = require('../config/config')
 
 const loginController = {
 
     // GET index
     showLoginForm: (req, res) => {
-        const error = req.query.error || null; // Captura a mensagem de erro da query string
-        res.render('login', { error }); // Passa a variável error para a view
+        const error = req.query.error || null // Captura a mensagem de erro da query string
+        res.render('login', { error }) // Passa a variável error para a view
     },
 
     //  login   
     login: (req, res) => {
-        const { username, password } = req.body;
+        const { username, password } = req.body
         if (username === USERNAME && password === PASSWORD_HASH) {
             // Cria um token
-            const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' }); // O token expira em 1 hora
-            res.json({ token });
+            const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' }) // O token expira em 1 hora
+            res.json({ token })
+            res.redirect('edit-episode')
         } else {
             // Redireciona para a página de login com uma mensagem de erro
-            return res.redirect('/login?error=Credenciais inválidas');
+            return res.redirect('/login?error=Credenciais inválidas')
         }
     }
 
