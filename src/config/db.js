7f187e09f329b4ec9697358bb83b30d5f440b367
Affonso Mesquita <102181682+Affmesquita@users.noreply.github.com>
2025-01-24 18:21:21 -0300

    update 240125 product on

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,16 @@
-// db.js
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://affonsomesquita:c5VhoKyNCuz3hNZA@ddp.abvyc.mongodb.net/?retryWrites=true&w=majority&appName=DDP"
-
+const uri = "mongodb://localhost:27017/radioddp"; // Substitua "nome_do_banco" pelo nome do seu banco
 
 // Função para conectar ao banco de dados
 async function connectToDatabase() {
   try {
-    await mongoose.connect(uri) // Remova as opções obsoletas
-    console.log("Conectado ao MongoDB com Mongoose!")
+    await mongoose.connect(uri); // Não precisa mais de opções obsoletas
+    console.log("Conectado ao MongoDB com Mongoose!");
   } catch (error) {
-    console.error("Erro ao conectar ao MongoDB:", error)
+    console.error("Erro ao conectar ao MongoDB:", error);
   }
 }
 
 // Exporta a função de conexão
-module.exports = { connectToDatabase }
+module.exports = { connectToDatabase };
