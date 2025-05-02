95a2b5922f4c13ae1710e6d664475e91b03c77d2
Affonso Mesquita <102181682+Affmesquita@users.noreply.github.com>
2025-05-02 14:46:49 -0300

    update 020525

--- src/middleware/multerConfig.js
+++ src/middleware/multerConfig.js
@@ -3,14 +3,12 @@ const path = require('node:path')
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        if (file.mimetype.startsWith('video/')) {
-            // Diretório para vídeos
-            cb(null, path.join(__dirname, '../../public/update-videos')); 
-        } else if (file.mimetype.startsWith('image/')) {
-            // Diretório para imagens
-            cb(null, path.join(__dirname, '../../public/update-images')); 
+        if (file.fieldname === 'video') {
+            cb(null, path.join(__dirname, '../../public/update-videos'))
+        } else if (file.fieldname === 'img') {
+            cb(null, path.join(__dirname, '../../public/update-images'))
         } else {
-            cb(new Error('Tipo de arquivo não suportado'), false);
+            cb(new Error('Campo de arquivo desconhecido'), false)
         }
     },
     filename: (req, file, cb) => {
@@ -18,8 +16,22 @@ const storage = multer.diskStorage({
     }
 })
 
+const fileFilter = (req, file, cb) => {
+    const videoTypes = ['video/mp4', 'video/webm', 'video/ogg']
+    const imageTypes = ['image/jpeg', 'image/png', 'image/webp']
+
+    if (file.fieldname === 'video' && videoTypes.includes(file.mimetype)) {
+        cb(null, true)
+    } else if (file.fieldname === 'img' && imageTypes.includes(file.mimetype)) {
+        cb(null, true)
+    } else {
+        cb(new Error('Tipo de arquivo não suportado'), false)
+    }
+}
+
+
 // Criar a instância do multer com a configuração de armazenamento
-const upload = multer({ storage,
+const upload = multer({ storage, fileFilter, 
     limits: { fileSize: 10 * 1024 * 1024 * 1024 }})
 
 // Exportar a instância do multer
