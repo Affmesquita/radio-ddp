60c39a35106a806ed6d89b839f04d42af1b94bef
Affonso Mesquita <102181682+Affmesquita@users.noreply.github.com>
2025-08-13 16:05:54 -0300

    update 130825

--- public/javaScript/slider.js
+++ public/javaScript/slider.js
@@ -14,7 +14,7 @@ export function sliderFunction() {
             divs[currentIndex].classList.add('active');
             
             if (divs[currentIndex].id === 'div1') {
-                header.style.backgroundImage = "url('/image/capas/capaz radio 2.png')";
+                header.style.backgroundImage = "url('/image/microfone-1.jpg')";
             } else if (divs[currentIndex].id === 'div2') {
                 header.style.backgroundImage = "url('/image/passofundo.jpg')";
             }
